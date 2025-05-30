238408f66a3dd34db88f852b962b5b172e583621
EmonHossen10 <emonhossen1000@gmail.com>
2025-05-30 11:47:35 +0600

    hero pioc added

--- src/Components/HeroSection/HeroPic.jsx
+++ src/Components/HeroSection/HeroPic.jsx
@@ -3,14 +3,12 @@ import { PiHexagonThin } from "react-icons/pi";
 
 const HeroPic = () => {
   return (
-    <div className=" h-full  flex items-center justify-center  "> 
+    <div className=" h-full  flex items-center justify-center  ">
       <img
-       
-        className="max-h-[500px] w-auto   "
-        src="../../public/images/emon2.jpg"
+        className="max-h-[500px] w-auto  rounded-xl  "
+        src="../../public/images/emon4.jpg"
         alt=""
       />
-  
     </div>
   );
 };
