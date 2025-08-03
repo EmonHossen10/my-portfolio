f8a0a31e4d4ac022cfe31ce51a199135beb42b22
EmonHossen10 <emonhossen1000@gmail.com>
2025-08-03 19:44:04 +0600

    sub skills added

--- src/Components/SkillsSections/SubSkills.jsx
+++ src/Components/SkillsSections/SubSkills.jsx
@@ -4,7 +4,7 @@ import img from "../../../public/images/subSkills.jpg"
 const SubSkills = () => {
   return (
     <div className="border-y-2 border-lightGrey relative ">
-      <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
+      <div className="absolute bg-gradient-to-r from-cyan to-lightCyan opacity-50 w-full h-full"></div>
       <img
         src={img}
         alt="subSkills image"
